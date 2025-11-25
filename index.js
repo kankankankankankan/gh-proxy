82d83ac8cad6dd2797135c34084c3f5f46644f23
kankankankankankan <vip029@me.com>
2025-11-25 13:00:24 +0800

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const Config = {
 
 const whiteList = [] // 白名单，路径里面有包含字符的才会通过，e.g. ['/username/']
 
-/** @type {ResponseInit} */
+/** @type {RequestInit} */
 const PREFLIGHT_INIT = {
     status: 204,
     headers: new Headers({
@@ -23,14 +23,15 @@ const PREFLIGHT_INIT = {
     }),
 }
 
-
 const exp1 = /^(?:https?:\/\/)?github\.com\/.+?\/.+?\/(?:releases|archive)\/.*$/i
 const exp2 = /^(?:https?:\/\/)?github\.com\/.+?\/.+?\/(?:blob|raw)\/.*$/i
 const exp3 = /^(?:https?:\/\/)?github\.com\/.+?\/.+?\/(?:info|git-).*$/i
 const exp4 = /^(?:https?:\/\/)?raw\.(?:githubusercontent|github)\.com\/.+?\/.+?\/.+?\/.+$/i
 const exp5 = /^(?:https?:\/\/)?gist\.(?:githubusercontent|github)\.com\/.+?\/.+?\/.+$/i
 const exp6 = /^(?:https?:\/\/)?github\.com\/.+?\/.+?\/tags.*$/i
-const exp7 = /^(?:https?:\/\/)?api\.github\.com\/.+?\/.+?\/.*$/i
+const exp7 = /^(?:https?:\/\/)?api\.github\.com\/.*$/i
+const exp8 = /^(?:https?:\/\/)?git\.io\/.*$/i
+const exp9 = /^(?:https?:\/\/)?gitlab\.com\/.*$/i
 
 /**
  * @param {any} body
@@ -39,7 +40,7 @@ const exp7 = /^(?:https?:\/\/)?api\.github\.com\/.+?\/.+?\/.*$/i
  */
 function makeRes(body, status = 200, headers = {}) {
     headers['access-control-allow-origin'] = '*'
-    return new Response(body, { status, headers })
+    return new Response(body, {status, headers})
 }
 
 
@@ -63,7 +64,7 @@ addEventListener('fetch', e => {
 
 
 function checkUrl(u) {
-    for (let i of [exp1, exp2, exp3, exp4, exp5, exp6, exp7]) {
+    for (let i of [exp1, exp2, exp3, exp4, exp5, exp6, exp7, exp8, exp9]) {
         if (u.search(i) === 0) {
             return true
         }
@@ -78,19 +79,34 @@ async function fetchHandler(e) {
     const req = e.request
     const urlStr = req.url
     const urlObj = new URL(urlStr)
+    
+    console.log("in:" +urlStr)
+
     let path = urlObj.searchParams.get('q')
     if (path) {
         return Response.redirect('https://' + urlObj.host + PREFIX + path, 301)
     }
+
+    path = urlObj.href.substr(urlObj.origin.length + PREFIX.length)
+    console.log ("path:" + path)
+
+    // 判断有没有嵌套自己调用自己
+    const exp0 = 'https:/' + urlObj.host + '/'
+    console.log ("exp0:" + exp0)
+    while (path.startsWith(exp0)) {
+        console.log ("in while")
+        path = path.replace(exp0, '')
+    }
+    console.log ("path:" + path)
+
     // cfworker 会把路径中的 `//` 合并成 `/`
-    path = urlObj.href.slice(urlObj.origin.length + PREFIX.length).replace(/^https?:\/+/, 'https://')
-    if (
-        path.search(exp1) === 0 ||
-        path.search(exp3) === 0 ||
-        path.search(exp5) === 0 ||
-        path.search(exp6) === 0 ||
-        path.search(exp7) === 0
-        ) {
+    path = path.replace(/^https?:\/+/, 'https://')
+    console.log ("path:" + path)
+
+    if (path.search(exp1) === 0 || path.search(exp3) === 0 || path.search(exp4) === 0 || path.search(exp5) === 0 || path.search(exp6) === 0 || path.search(exp7) === 0 || path.search(exp8) === 0 || path.search(exp9) === 0) {
+        
+        console.log("exp 1,3,4,5,6,7,8, 9")
+
         return httpHandler(req, path)
     } else if (path.search(exp2) === 0) {
         if (Config.jsdelivr) {
@@ -101,14 +117,23 @@ async function fetchHandler(e) {
             return httpHandler(req, path)
         }
     } else if (path.search(exp4) === 0) {
-        if (Config.jsdelivr) {
-            const newUrl = path.replace(/(?<=com\/.+?\/.+?)\/(.+?\/)/, '@$1').replace(/^(?:https?:\/\/)?raw\.(?:githubusercontent|github)\.com/, 'https://cdn.jsdelivr.net/gh')
-            return Response.redirect(newUrl, 302)
-        }
-        else {
-            return httpHandler(req, path)
-        }
+        const newUrl = path.replace(/(?<=com\/.+?\/.+?)\/(.+?\/)/, '@$1').replace(/^(?:https?:\/\/)?raw\.(?:githubusercontent|github)\.com/, 'https://cdn.jsdelivr.net/gh')
+        return Response.redirect(newUrl, 302)
+    } else if (path==='perl-pe-para') {
+      let perlstr = 'perl -pe'
+      let responseText = 's#(bash.*?\\.sh)([^/\\w\\d])#\\1 | ' + perlstr + ' "\\$(curl -L ' + urlObj.origin + '/perl-pe-para)" \\2#g; ' +
+                   's# (git)# https://\\1#g; ' +
+                   's#(http.*?git[^/]*?/)#' + urlObj.origin + '/\\1#g';
+      return new Response( responseText, { status: 200, 
+            headers: {
+              'Content-Type': 'text/plain',
+              'Cache-Control': 'max-age=300'
+            }
+          });
     } else {
+        
+        console.log("fetch " + ASSET_URL + path)
+
         return fetch(ASSET_URL + path)
     }
 }
@@ -139,11 +164,14 @@ function httpHandler(req, pathname) {
         }
     }
     if (!flag) {
-        return new Response("blocked", { status: 403 })
+        return new Response("blocked", {status: 403})
     }
-    if (urlStr.search(/^https?:\/\//) !== 0) {
+    if (urlStr.startsWith('git')) {
         urlStr = 'https://' + urlStr
     }
+
+    console.log("urlStr "+urlStr)
+
     const urlObj = newUrl(urlStr)
 
     /** @type {RequestInit} */
